bfd1d509dcd170fd01fa81ad5006150cc5ba1100
aigul <a.aigul757@gmail.com>
2023-02-15 00:01:25 +0500

    CR

--- src/Emloyee.java
+++ src/Emloyee.java
@@ -1,11 +1,11 @@
 public class Emloyee {
-    private static  int counterId =1;
+    private static int counterId = 1;
     private final int id;
     private final String fullName;
     private int departmentId;
     private int salary;
 
-    public Emloyee (String fullName, int departmentId,int salary) {
+    public Emloyee(String fullName, int departmentId, int salary) {
         this.id = counterId++;
         this.fullName = fullName;
         this.departmentId = departmentId;
@@ -15,24 +15,30 @@ public class Emloyee {
     public void setDepartmentId(int departmentId) {
         this.departmentId = departmentId;
     }
+
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
     public int getId() {
         return id;
     }
+
     public String getFullName() {
         return fullName;
     }
+
     public int getDepartmentId() {
         return departmentId;
     }
+
     public int getSalary() {
         return salary;
     }
+
     @Override
-    public String toString () {
-        return String.format ("id: %d, ФИО: %s, ЗП: %d, № отдела: %d",
+    public String toString() {
+        return String.format("id: %d, ФИО: %s, ЗП: %d, № отдела: %d",
                 id,
                 fullName,
                 salary,
